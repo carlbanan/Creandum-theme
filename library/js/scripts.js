b05daae4e130a2a398ef2f3699e4b6bfced1263a
carlwetzenstein <carlwetzenstein@gmail.com>
2013-11-14 15:13:41 +0100

    ticker

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -339,7 +339,8 @@ $('span.one').hover(function () {
 */
 
 
-    $('.one').hover(function(){     
+    $('.one').hover(function(){
+    	console.log('sdf')
         $('.tick').addClass('ticker_margin_one');    
     },     
     function(){    
@@ -360,12 +361,12 @@ $('span.one').hover(function () {
         $('.tick').removeClass('ticker_margin_three');     
     });
 
+
     console.log('fdsf')
-	var hello = setInterval(function() {
+	setInterval(function() {
 		console.log('hello world')
 	}, 5000) 
 
-	hello();
 
 
 
