37c0a63612306545b0e24d5886a4f96b6903e734
pontusw <p.wiehager@gmail.com>
2013-11-21 19:12:08 +0100

    Team filter

--- archive-team.php
+++ archive-team.php
@@ -1,55 +1,86 @@
 <?php get_header(); ?>
 
-	<div id="async_content">
-		
-		<!-- AJAX CONTENT GOES HERE -->
-		<?php
-			if(SINGLEPOST == 1){
-				$_template_file = get_template_directory()."/single-team-content.php";
-				load_template( $_template_file);
-			}
-			else{
-				?>
-		<!-- LIST VIEW PAGE / FIRST PAGE INVEST CONTENT -->
-		<div class"top-post">
-				<!-- Big picture -->
-				<div class="page-content intro-about">	
-								<!--<p class="large_invest_white"></p> -->
-								<section class="post_content center clearfix" itemprop="articleBody">
-								<h2>
-								 This is team Creandum and also a great part of somewhat 60 companies. 
-								</h2>
-								</section>
-						</div>
-						<div class="responsive-wrapper">
-							<div class="big-image-container"  style='background-image:url(<?php echo get_template_directory_uri(); ?>/images/group.jpg);'></div>
-							<div class="green_background"></div>
-						</div>	
-				</div>	
-	</div>	
-	<?php } ?>
+<div id="async_content">
+	
+	<!-- AJAX CONTENT GOES HERE -->
+	<?php
+		if(SINGLEPOST == 1){
+			$_template_file = get_template_directory()."/single-team-content.php";
+			load_template( $_template_file);
+		}
+		else{
+			?>
+	<!-- LIST VIEW PAGE / FIRST PAGE INVEST CONTENT -->
+	<div class"top-post">
+			<!-- Big picture -->
+			<div class="page-content intro-about">	
+							<!--<p class="large_invest_white"></p> -->
+							<section class="post_content center clearfix" itemprop="articleBody">
+							<h2>
+							 This is team Creandum and also a great part of somewhat 60 companies. 
+							</h2>
+							</section>
+					</div>
+					<div class="responsive-wrapper">
+						<div class="big-image-container"  style='background-image:url(<?php echo get_template_directory_uri(); ?>/images/group.jpg);'></div>
+						<div class="green_background"></div>
+					</div>	
+			</div>	
+</div>	
+<?php } ?>
 
-	</div>
-	<div id="hidden_content">
-		<!-- HIDDEN CONTENTN IS STORED HERE -->
-	</div>
-
-	<div class='lightgreen_bg padblock infront'>
-
-		<div class="container">
+</div>
+<div id="hidden_content">
+	<!-- HIDDEN CONTENTN IS STORED HERE -->
+</div>
+<div class="team">
+	<div class='lightgreen_bg infront'>
 
 			<div id="content" page='team' class="clearfix clear">
 			
 				<div id="main" class="clearfix" role="main">
 
-					<div class="row">
 
 						<?php
 						  query_posts( array( 'post_type' => 'team') );
+						  $team = array();
+						  $menuitem = array();
 						  if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+						  	// FOR MENU
+						    $pos = $posclass = "";
+						  	$pos = get_post_meta($post->ID,"custom_title",true);
+						  	$posclass = str_replace(" ","-",$pos);
+						  	$menuitem[$posclass] = $pos;
+
+						  	// FOR LISTING
+						  	$post->cotitle = $posclass;
+						  	array_push($team,$post);
+
+						  endwhile;
+						  endif;
+							  if($menuitem){
+							  	?>
+								<div class='filter-menu'>
+									<span class='filter active' id=''>All</span>
+							  	<?php
+								  	foreach($menuitem as $key => $value){
+								  		echo '<span class="filter" id="'.$key.'">' .$value. '</span>';
+								  	}
+							  	?>
+							  	</div>	<!--END FILTER -->
+							  	<?php
+							  }
+					?>
+				<div class="container">	
+					<div class="row">
+					<?php
+						  if($team){
+						  	foreach($team as $post){
 						?>
-												
-						<div id="post-<?php the_ID(); ?>" <?php post_class('col-md-3  col-lg-3  col-sm-6'); ?> role="article">
+
+
+						<div id="post-<?php the_ID(); ?>" <?php post_class('col-md-3  col-lg-3  col-sm-6 filterable '.$post->cotitle); ?> role="article">
 								
 							<a href="<?php the_permalink() ?>"  select="post_<?php the_ID();?>" class='async' rel="bookmark" title="<?php the_title_attribute(); ?>">		
 									
@@ -70,7 +101,7 @@
 							</a>
 						</div> <!-- end article -->
 						
-						<?php endwhile; ?>	
+						<?php } ?>	
 						
 						<?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
 							
@@ -86,7 +117,7 @@
 						<?php } ?>
 									
 						
-						<?php else : ?>
+						<?php }else{ ?>
 						
 						<article id="post-not-found">
 						    <header>
@@ -99,7 +130,7 @@
 						    </footer>
 						</article>
 						
-						<?php endif; ?>
+						<?php } ?>
 				
 					</div> <!-- end row -->
 					
@@ -110,5 +141,7 @@
 		</div> <!-- end container -->
 	
 	</div> <!--end lightgreen_bg-->
+
+</div> <!-- END TEAM -->
 
 <?php get_footer(); ?>
